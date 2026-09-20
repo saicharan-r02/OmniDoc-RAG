47daaad4fcf5225b0f37818e927a41e364563f35
saicharan-r02 <saicharanr227@gmail.com>
2026-09-20 23:06:09 +0530

    refactor: simplify API package exports

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,3 @@
-"""
-API and programmatic routes package for OmniDoc-RAG
-"""
+from .routes import answer_query,query_pipeline
 
-from .routes import answer_query, query_pipeline
-
-__all__ = ["answer_query", "query_pipeline"]
+__all__=["answer_query","query_pipeline"]
