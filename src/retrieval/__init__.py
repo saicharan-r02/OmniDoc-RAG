0655ed9bb981fa5925fbaf800b6274fd051195b8
saicharan-r02 <saicharanr227@gmail.com>
2026-09-22 20:50:15 +0530

    refactor: simplify retrieval package exports

--- src/retrieval/__init__.py
+++ src/retrieval/__init__.py
@@ -1,17 +1,3 @@
-"""
-Retrieval package for OmniDoc-RAG
-"""
+from .retriever import expand_query,get_related_subjects,retrieve_context,is_context_relevant
 
-from .retriever import (
-    expand_query,
-    get_related_subjects,
-    retrieve_context,
-    is_context_relevant,
-)
-
-__all__ = [
-    "expand_query",
-    "get_related_subjects",
-    "retrieve_context",
-    "is_context_relevant",
-]
+__all__=["expand_query","get_related_subjects","retrieve_context","is_context_relevant"]
