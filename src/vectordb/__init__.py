6f4600b8dfbd148755ec7c33326c345f9f185fa5
saicharan-r02 <saicharanr227@gmail.com>
2026-09-22 21:22:12 +0530

    refactor: simplify vector database package exports

--- src/vectordb/__init__.py
+++ src/vectordb/__init__.py
@@ -1,7 +1,3 @@
-"""
-Vector database package for OmniDoc-RAG
-"""
+from .vector_store import VectorStoreManager,get_vector_store,get_subject_counts,ensure_vector_db_ready
 
-from .vector_store import VectorStoreManager, get_vector_store, get_subject_counts, ensure_vector_db_ready
-
-__all__ = ["VectorStoreManager", "get_vector_store", "get_subject_counts", "ensure_vector_db_ready"]
+__all__=["VectorStoreManager","get_vector_store","get_subject_counts","ensure_vector_db_ready"]
